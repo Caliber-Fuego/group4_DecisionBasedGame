475a2cc7e818cc12ce141050f5253892fa5fae2a
Asus <louisfuego03@gmail.com>
2022-02-02 18:45:15 +0800

    Added Hero Stats page with HP Values Health Points can now be displayed on the Game Screen

--- app/src/main/java/com/example/group4_decisionbasedgame/GameScreen.java
+++ app/src/main/java/com/example/group4_decisionbasedgame/GameScreen.java
@@ -2,6 +2,7 @@ package com.example.group4_decisionbasedgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +10,11 @@ import android.widget.TextView;
 
 public class GameScreen extends AppCompatActivity {
 
-    TextView text;
+    TextView text, hptext;
     Button btn1, btn2;
 
     Story story = new Story(this);
-
+    HeroStats heroStats = new HeroStats(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +22,16 @@ public class GameScreen extends AppCompatActivity {
         setContentView(R.layout.activity_game_screen);
 
         text = (TextView)findViewById(R.id.storyText);
+        hptext = (TextView)findViewById(R.id.healthDisplay);
+
+        hptext.setText(String.valueOf(heroStats.heroHPoints));
+
         btn1 = (Button) findViewById(R.id.btnchoice01);
         btn2 = (Button) findViewById(R.id.btnchoice02);
 
         story.startingPoint();
+
+
     }
 
     public void btn1 (View view){
@@ -35,6 +42,14 @@ public class GameScreen extends AppCompatActivity {
         story.selectPosition(story.nextPosition2);
 
     }
+
+    public void viewHeroStats (View v){
+
+        Intent herostats = new Intent(this, HeroStats.class);
+        startActivity(herostats);
+    }
+
+
 
 
 }
--- app/src/main/java/com/example/group4_decisionbasedgame/Story.java
+++ app/src/main/java/com/example/group4_decisionbasedgame/Story.java
@@ -6,6 +6,7 @@ public class Story {
     GameScreen gs;
     String nextPosition1, nextPosition2;
 
+
     public Story(GameScreen gs) {
 
         this.gs = gs;
@@ -15,17 +16,18 @@ public class Story {
         switch(position){
             case "startingPoint": startingPoint(); break;
             case "gameOver": gameOver(); break;
+            case "oldMan": oldMan(); break;
         }
     }
     public void startingPoint(){
 
-        gs.text.setText("You are at the Demon Lord Castle Gate.\n " +
+        gs.text.setText("You are at the Demon Lord Castle Gate.\n" +
                         "what will you do?");
 
         gs.btn1.setText("Open the Gate");
         gs.btn2.setText("Go back and sleep");
 
-        nextPosition1 = "lootchest";
+        nextPosition1 = "oldMan";
         nextPosition2 = "gameOver";
     }
 
@@ -39,4 +41,17 @@ public class Story {
         nextPosition2 = "";
 
     }
+    public void oldMan(){
+        gs.text.setText("You encounter an old man wearing a robe.\n" +
+                        "'It's dangerous to go alone, take this.'\n \n" +
+                        "He hands you over a sword, do you accept?");
+
+        gs.btn1.setText("Take the Sword");
+        gs.btn2.setText("Go back outside");
+
+        nextPosition1 = "";
+        nextPosition2 = "startingPoint";
+    }
+
+
 }
